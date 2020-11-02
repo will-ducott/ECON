228d8ce76da15f8c3ec14884baeb16a4873f0128
William Ducott <73796835+will-ducott@users.noreply.github.com>
2020-11-02 16:38:48 -0500

    Update Basic Micro.R

--- Basic Micro.R
+++ Basic Micro.R
@@ -66,8 +66,61 @@ MrgCost <- function(deltaTC , deltaQ){
 ProfitMaxQ <- function(deltaTR, deltaTC, deltaQ){
   MR <- deltaTR/deltaQ
   MC <- deltaTC/deltaQ
-  if (MR=MC){
+  if (MC == MR){
     Q <- MR
+    prc <- "Price where Q is maximized"
+  print(prc)
   return(Q)}
+  else if (MC != MR){
+    x <- "NOT MAXIMIZED"
+    print(x)}
+  }
+
+Shutdown <- function(TR,Q,TVC){
+  P <- TR/Q
+  AVC <- TVC/Q
+  if (P == AVC){
+    sd <- "SHUTDOWN"
+    print(sd)
+    return(P)}
+  else if (P != AVC){
+    stay <- "STAY OPEN"
+    print(stay)}
+}
+
+Breakeven <- function(TR, TC, Q){
+  AC <- TC/Q
+  P <- TR/Q
+  if (P == AC){
+    bep <- "BREAKEVEN"
+    print(bep)
+    return(P)}
+  else if (P != AC){
+    nbep <- "NOT BREAKEVEN POINT"
+    print(nbep)}
+}
+
+ProfitAnalysis <- function(TR,TC,Q,TVC=NULL){
+  P <- TR/Q
+  AVC <- TVC/Q
+  AC <- TC/Q
+  if (P > AC){
+    prft <- "PROFIT"
+    gain <- (P-AC)*Q
+    print(prft)
+    return(gain) }
+  if (P < AC){
+    loss <- "LOSS"
+    neg <- (P-AC)*Q
+    print(loss)
+    return(neg)}
+  if (P == AC){
+    bep <- "BREAKEVEN"
+    be <- 0
+    print(bep)
+    return(be)}
+  if (P == AVC){
+    sd <- "SHUTDOWN"
+    print(sd)}
 }
 
